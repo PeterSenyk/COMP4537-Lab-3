e06f1252f30004bfd07e96349c637106c7f0aac4
Peter <Peter.Senyk@gmail.com>
2025-09-27 17:58:40 -0700

    feat: add math module

---
+++ app.js
@@ -0,0 +1,10 @@
+const Math = require('./math');
+
+const a = 22;
+const b = 11;
+
+const sum = Math.add(a, b);
+console.log(`Hello Peter, the sum of ${a} and ${b} is ${sum}`);
+
+const difference = Math.subtract(a, b);
+console.log(`Hello Peter, the difference of ${a} and ${b} is ${difference}`);
